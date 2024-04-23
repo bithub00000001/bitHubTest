6426fe6783652d8ff4afe0e7fc7bc7d597358e78
ksmart <dmdm9218@jbnu.ac.kr>
2024-04-23 11:59:39 +0900

    [team01] 240423 새로운 프로젝트 생성

---
+++ src/test/java/ksmart/githubtest/GitHubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ksmart.githubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
